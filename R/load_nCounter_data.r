28ef2571eb212970cd0ed59b3920fb1168341b45
lwhitmore <leanne382@gmail.com>
2022-10-31 16:31:47 -0700

    update options in load_nCount_files function

--- R/load_nCounter_data.r
+++ R/load_nCounter_data.r
@@ -2,7 +2,7 @@
 #'
 #' This loads count and target file info (internal function only)
 #' @param pathtoRCC  matrix raw or normalized (default is current working directory)
-#' @param target_file file with experimental information (meta data csv file)
+#' @param meta.data file with experimental information (meta data csv file)
 #' @keywords Ncounter RCC files
 #' @export
 #' @import NanoStringQCPro
