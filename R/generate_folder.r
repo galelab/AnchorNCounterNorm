f380dda31defebfbe35fa6a2d32eb757ccb098f9
lwhitmore <leanne382@gmail.com>
2023-11-16 14:32:06 -0800

    add new file with generate folder function

---
+++ R/generate_folder.r
@@ -0,0 +1,16 @@
+#' generates folder to put results
+#'
+#' @param foldername  name of folder (directory) to generate
+#' @examples
+#' generate_folder("directoryname")
+
+generate_folder <- function(foldername) {
+    workDir <- getwd()
+    subDir <- foldername
+    results_path <- file.path(workDir, subDir)
+    if (file.exists(subDir)) {
+    } else {
+        dir.create(results_path)
+    }
+    return(results_path)
+}
