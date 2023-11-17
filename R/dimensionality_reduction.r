67f4b424c13f2ee2028cdfbbfe40d8cc3d08fd86
lwhitmore <leanne382@gmail.com>
2023-11-17 14:40:35 -0800

    fix bugs in dimensionality_reduction code

--- R/dimensionality_reduction.r
+++ R/dimensionality_reduction.r
@@ -5,7 +5,7 @@
 #' @keywords house keeping genes
 #' @export
 #' @import factoextra
-#' @import Rtsne
+#' @import umap
 #' @import ggplot2
 #' @import Polychrome
 #' @examples
@@ -13,42 +13,48 @@
 #' 
 #' 
 
-dim_reduction <- function(exprs, meta.data,  target_columns=c(1,2),
+dim_reduction <- function(exprs, meta.data,  target_columns=c(2,3),
     reduction="PCA", save.fig=TRUE, output_dir=getwd(), pointsize=3) {
-    c1ass1 = meta.data[, target_columns[1]]
+
+    class1 = meta.data[, target_columns[1]]
     class2 = meta.data[, target_columns[2]]
     P36 <- createPalette(length(levels(factor(class2))), c("#ff0000", "#00ff00", "#0000ff"))
 
     if (reduction=="PCA") {
         pca <- prcomp(t(exprs))
         E <- get_eig(pca)
-        cx <- sweep(t(exprs), 2, colMeans(t(exprs)), "-")
         PCA <- pca$x
-        minx <- min(PCA[, 1])
-        maxx <- max(PCA[, 1])
-        miny <- min(PCA[, 2])
-        maxy <- max(PCA[, 2])
 
-        pcaplot <- ggplot(aes(x=PCA[, 1], y=PCA[, 2], color = factor(class2), shape = factor(class1), size = I(pointsize))) +
+        df <- as.data.frame(cbind(PCA[, 1], PCA[, 2], class1, class2))
+        colnames(df) <- c("PC1", "PC2", "class1", "class2")
+        df$PC1 <- as.numeric(df$PC1)
+        df$PC2 <- as.numeric(df$PC2)
+
+        pcaplot <- ggplot(df, aes(x=PC1, y=PC2, color = factor(class2), 
+            shape = factor(class1))) +
+            geom_point(size = I(pointsize)) +
             theme_minimal() +
             theme(legend.title = element_blank()) +
             xlab(paste0("PC1 ", round(E$variance.percent[1], digits = 2), "%")) +
             ylab(paste0("PC2 ", round(E$variance.percent[2], digits = 2), "%")) +
-            theme(legend.position = "right") +
+            theme(legend.position = "right") + theme(legend.key.size = unit(0.2, "cm"),
+                panel.grid.major = element_line(colour = "#f0f0f0"),
+                panel.grid.minor = element_blank(), 
+                axis.line = element_line(colour = "black", linewidth=0.2)) +
             scale_color_manual(values = as.character(P36)) +
             scale_fill_manual(values = as.character(P36)) +
             scale_shape_manual(values = seq(1, length(levels(factor(class1)))))
         if (isTRUE(save.fig)) {
-            ggsave(ggsave(file.path(output_dir, paste0(reduction,".png")), width = 4.5, height = 3, bg = "white", dpi = 300))
-            ggsave(ggsave(file.path(output_dir, paste0(reduction, ".pdf")), width = 4.5, height = 3, bg = "white", dpi = 300))
+            ggsave(file.path(output_dir, paste0(reduction,".png")), width = 4.5, height = 3, bg = "white", dpi = 300)
+            ggsave(file.path(output_dir, paste0(reduction, ".pdf")), width = 4.5, height = 3, bg = "white", dpi = 300)
         }
         #scree plot 
-        scree.plot <- fviz_eig(PCA, addlabels = TRUE, hjust = -0.3)
+        scree.plot <- fviz_eig(pca, addlabels = TRUE, hjust = -0.3)
         if (isTRUE(save.fig)) {
             png(file.path(output_dir, paste0(reduction, "scree.png")), width = 7, height = 6, units = "in", res = 300)
             print(scree.plot)
             dev.off()
-            pdf(file.path(output_dir, paste0(reduction, "scree.pdf")), width = 7, height = 6, units = "in", res = 300)
+            pdf(file.path(output_dir, paste0(reduction, "scree.pdf")), width = 7, height = 6)
             print(scree.plot)
             dev.off()
         }
@@ -67,23 +73,27 @@ dim_reduction <- function(exprs, meta.data,  target_columns=c(1,2),
         loadingscores <- loadingscores[with(loadingscores, order(-PC2)), ]
         write.table(loadingscores["PC2"], file = file.path(output_dir, "loadingscores_pc2.txt"))
     } else if (reduction=="UMAP") {
-        UMAP <- umap(t(exprs))
-        U <- umap$layout
-        minx <- min(U[, 1])
-        maxx <- max(U[, 1])
-        miny <- min(U[, 2])
-        maxy <- max(U[, 2])
-
-        umapplot <- ggplot(aes(x = U[, 1], y = U[, 2], color = factor(class2), shape = factor(class1), size = I(pointsize))) +
+        UMAP <- umap(t(exprs), n_neighbors=5)
+        U <- UMAP$layout
+        df <- as.data.frame(cbind(U[, 1], U[, 2], class1, class2))
+        colnames(df) <- c("UMAP1", "UMAP2", "class1", "class2")
+        df$UMAP1 <- as.numeric(df$UMAP1)
+        df$UMAP2 <- as.numeric(df$UMAP2)
+        umapplot <- ggplot(df, aes(x = UMAP1, y = UMAP2, color = factor(class2), 
+            shape = factor(class1))) +
+            geom_point(size=pointsize) +
             theme_minimal() +
             theme(legend.title = element_blank()) +
-            theme(legend.position = "right") +
+            theme(legend.position = "right") + theme(legend.key.size = unit(0.2, "cm"),
+                panel.grid.major = element_line(colour = "#f0f0f0"),
+                panel.grid.minor = element_blank(), 
+                axis.line = element_line(colour = "black", linewidth=0.2)) + 
             scale_color_manual(values = as.character(P36)) +
             scale_fill_manual(values = as.character(P36)) +
             scale_shape_manual(values = seq(1, length(levels(factor(class1)))))
         if (isTRUE(save.fig)) {
-            ggsave(ggsave(file.path(output_dir, paste0(reduction, ".png")), width = 4.5, height = 3, bg = "white", dpi = 300))
-            ggsave(ggsave(file.path(output_dir, paste0(reduction, ".pdf")), width = 4.5, height = 3, bg = "white", dpi = 300))
+            ggsave(file.path(output_dir, paste0(reduction, ".png")), width = 4.5, height = 3, bg = "white", dpi = 300)
+            ggsave(file.path(output_dir, paste0(reduction, ".pdf")), width = 4.5, height = 3, bg = "white", dpi = 300)
         }
     } else {
         stop("ERROR: reduction parameter has to set to PCA or UMAP")
